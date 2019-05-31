934d1357c1d084dcb737707977c082b599230ff4
saivivek1987 <51218020+saivivek1987@users.noreply.github.com>
2019-05-30 21:09:44 -0400

    First commit hurray!!!
    LinkedList

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+struct node {
+	int data;
+	node *next;
+	node(int value)	{
+		data = value;
+		next = NULL;}
+	node ()
+	{ data = 0;
+		next = NULL;	}
+};
+
+
+node *start = NULL;
+
+void addNode(int data)
+{
+	node *temp = new node(data);
+	
+	if ( start == NULL)
+	{
+		start = temp;
+	}
+	else
+	{
+		node *curr = start;
+		
+		for (;curr->next != NULL; curr = curr->next)
+		{
+		//	cout<< curr->data <<endl;
+		}
+		curr->next = temp;
+	}
+}
+
+
+void display ()
+{
+		node *curr = start;
+		
+		for (;curr->next != NULL; curr = curr->next)
+		{
+			cout<< "\t\t\t\t\t"<<curr->data << endl;
+		}
+		cout<< "\t\t\t\t\t"<<curr->data << endl;
+		
+}
+
+int main() {
+	// your code goes here
+	
+
+	for (int i = 1; i <= 10; ++i)
+	{
+		addNode(i);
+	}
+	
+	display();
+	
+	return 0;
+}
